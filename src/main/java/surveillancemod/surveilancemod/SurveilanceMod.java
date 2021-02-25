74be205f7f8e330b1a784d1f3b2bfefdabc3f3b6
moop7 <66098903+moop7@users.noreply.github.com>
2021-02-25 17:53:04 -0500

    all files needed
    Adding java code

---
+++ src/main/java/surveillancemod/surveilancemod/SurveilanceMod.java
@@ -0,0 +1,10 @@
+package surveillancemod.surveilancemod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SurveilanceMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
